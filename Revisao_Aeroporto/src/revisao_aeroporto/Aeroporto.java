d3a2f8d86d528c7739e585a9d968f1073a294705
Vitor Salzman <vitor-salzman96@hotmail.com>
2018-08-06 20:58:37 -0300

    POO2 - Revisao_Aeroporto
    Continuação exercício

--- Revisao_Aeroporto/src/revisao_aeroporto/Aeroporto.java
+++ Revisao_Aeroporto/src/revisao_aeroporto/Aeroporto.java
@@ -5,13 +5,65 @@
  */
 package revisao_aeroporto;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 20161bsi0403
  */
 
 public class Aeroporto {
-     public int codigo;
-     public String nome;
+     private int codigo;
+     private String nome;
+     private boolean isInternacional = true;
+     private ArrayList<Aeroporto> destinosDiretos;
+     private ArrayList<Aeroporto> origensDiretas;
+     private ArrayList<Aviao> avioesNoPatio;
+     
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+     
+    public boolean isInternacional(){
+        return this.isInternacional;
+    } 
     
+    public void setIsInternacional(boolean situacao){
+        this.isInternacional=situacao;
+    }
+    
+    public boolean equals(Aeroporto aero, Aeroporto aero2){
+        return aero.getCodigo()==aero2.getCodigo(); 
+            
+    }
+
+    public ArrayList<Aviao> getAvioesNoPatio() {
+        return avioesNoPatio;
+    }
+    
+    
+    
+    public boolean estaPousado(Aeroporto aero,String prefixo){
+        
+        ArrayList<Aviao> listAvioes=aero.getAvioesNoPatio();
+        for(int i=0; i<listAvioes.size();i++){
+            if(listAvioes.get(i).getPrefixo()==prefixo){
+                return true;
+            }    
+        }
+        return false;
+        
+    }
 }
